c32bb8801649576f37ed4bc1f713d33299536563
rossstore <racapones2316@gmail.com>
2024-09-25 19:18:11 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package forloopdialognirosss;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Student
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String ross=JOptionPane.showInputDialog("welcome to ross store how can i help you?");
+        int alfred=Integer.parseInt(ross);
+       JOptionPane.showMessageDialog(null, alfred+10+"wow naa diri si r.a hahaah \n , naa napud ang patisoy0") ;
+    }
+
+}
